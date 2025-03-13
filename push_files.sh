48733a10882e829808852b7eac889ed0c9d967e4
Noatmeal <noatmeal94@gmail.com>
2025-03-13 07:20:36 -0500

    Testing pushing files.

---
+++ push_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+filepaths=()
+
+for FILE in "site"/*; do
+  RELATIVE_PATH="${FILE#site/}"
+  filepaths+=("$RELATIVE_PATH")
+done
+
+if [ ${#filepaths[@]} -eq 0 ]; then
+  echo "No files to push."
+  exit 0
+fi
+
+curl_command="curl -H 'Authorization: Bearer $NEOCITIES_API_KEY'"
+
+for FILEPATH in "${filepaths[@]}"; do
+  curl_command+=" -F '$FILEPATH=@$FILEPATH'"
+done
+
+curl_command+=" 'https://neocities.org/api/upload'"
+
+echo "Running:"
+echo "$curl_command"
+
+# eval "$curl_command"
